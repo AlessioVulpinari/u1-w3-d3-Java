75c046dd3939b51c9f3a6fe88581ec5d5b89c4cf
Alessio Vulpinari <alessiovulpinari99@gmail.com>
2024-06-05 16:02:36 +0200

    Entity relations and creations
    - Event
    - Invite
    - Person
    - Location

--- src/main/java/alessiovulpinari/Application.java
+++ src/main/java/alessiovulpinari/Application.java
@@ -1,8 +1,14 @@
 package alessiovulpinari;
 
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Application {
 
+    private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("u1-w3-d3-Java");
+
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
     }
 }
